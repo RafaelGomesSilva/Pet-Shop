5a00d48b715814a7dee37e7e806b9317d64b5d6f
Rafael Gomes <rafaelurucania@gmail.com>
2025-08-04 19:17:08 -0300

    Atualização
    Ok

--- Exercicios/src/Main.java
+++ Exercicios/src/Main.java
@@ -1,8 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private final static scanner = new Scanner(System.in);
+    private final static PetMachine petMachine = new PetMachine();
+
 
     public static void main(String[] args) {
-        
+
+        var option = -1;
+        do {
+            System.out.println("===Escolha uma das opções===");
+            System.out.println("1 - Dar banho no pet");
+            System.out.println("2 - Abastecer a máquina com água");
+            System.out.println("3 - Abastecer a máquina com shampoo);
+            System.out.println("4 - Verificar água da máquina");
+            System.out.println("5 - Verificar se tem shampoo na máquina");
+            System.out.println("6 - Verificar se tem pet na máquina);
+            System.out.println("7 - Colocar o pet na máquina");
+            System.out.println("8 - Retirar o pet da máquina");
+            System.out.println("9 - Limpar a máquina");
+            System.out.println("0 Sair");
+            option = scanner.nextInt();
+            switch (option){
+            }
+                case 6 -> petMachine.hasPet();
+                case 7 -> setPetInPetMachine();
+                case 8 ->petMachine.removePet();
+                case 9 -> petMachine.wash();
+            }
+        }while (option !=0);
     }
+    public static void setPetInPetMachine(){
+        var name = "";
+        while (name == null || name.isEmpty()){
+        System.out.println ("Informe o nome do pet");
+        var name = scanner.next();
+
+        }
+
+        var pet = new Pet(name);
+        petMachine.setPet(pet);
+        System.out.println("O pet" + pet.getName() + "Foi colocado na máquina");
 }
---
+++ Exercicios/src/PetLavatório.java
@@ -0,0 +1,81 @@
+public class PetLavatório {
+
+    private boolean clean = true;
+
+    private int water = 30;
+
+    private int shampoo 10;
+
+    private Pet pet;
+
+
+    public void takeAshowerInAPet(){
+        if (this.pet == null){
+            System.out.println("Coloque o pet na máquina para iniciar o banho");
+            return;
+
+        }
+        this.water -= 10;
+        this.shampoo -= 2;
+    pet.setClean(true);
+        System.out.println("o pet" + pet.getName() + "Está limpinho");
+
+    }
+    public void addwater(){
+        if (water == 30){
+            System.out.println("A capacidade de agua da máquina está no limite");
+            return;
+        }
+        water += 2;
+    }
+
+    public void addShampoo(){
+        if (shampoo == 10){
+            System.out.println("A capacidade de shampoo na máquina está no limite");
+            return;
+        }
+        shampoo += 2;
+
+}
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getShampoo() {
+        return shampoo;
+
+        public boolean hasPet(){
+            return pet != null;
+        }
+
+
+    }
+
+    public void setPet(Pet pet) {
+        if (!this.clean){
+            System.out.println("A maquina esta suja, para colocar o pet é necesário limpa-la");
+        return;
+
+        }
+
+        if (hasPet()){
+            System.out.println("O pet " + this.pet.getName() + "Está na maquina nesse momento");
+         return;
+        }
+
+    }
+        this.pet = pet;
+    public void removePet(){
+        this.clean = this.pet.isClean();
+        System.out.println("O Pet" + this.pet.getName() + "Está Limpinho");
+        this.pet = null;
+    }
+
+    public void wash(){
+        this.water -= 10;
+        this.shampoo -= 2;
+        this.clean = true;
+        System.out.println ("Máquina super limpa!!! |°| ")
+    }
+    }
